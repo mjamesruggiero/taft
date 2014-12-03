77e3d2d75de7a6b43d2e03fb68d26c1bfc703826
mjamesruggiero <mjamesruggiero@gmail.com>
2014-12-02 20:54:28 -0800

    fix build dependencies

--- project/TaftBuild.scala
+++ project/TaftBuild.scala
@@ -1,7 +1,27 @@
 import sbt._
 import sbt.Keys._
+import com.github.bigtoast.sbtthrift.ThriftPlugin
+import sbtassembly.Plugin._
+import AssemblyKeys._
+
+object Dependencies {
+  val resolutionRepos = Seq(
+    "Snowplow Analytics Maven repo" at "http://maven.snplow.com/releases/",
+    "BintrayJCenter" at "http://jcenter.bintray.com",
+    "bigtoast-github" at "http://bigtoast.github.com/repo/",
+    "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
+    "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases"
+  )
+}
 
 object TaftBuild extends Build {
+  lazy val sbtAssemblySettings = assemblySettings ++ Seq(
+    // Slightly cleaner jar name
+    jarName in assembly := {
+      name + "-" + version + ".jar"
+    }
+  )
+
   val ScalaVersion = "2.10.3"
 
   lazy val taft = Project(
@@ -13,9 +33,7 @@ object TaftBuild extends Build {
       version := "0.1-SNAPSHOT",
       scalaVersion := ScalaVersion,
 
-      resolvers ++= Seq(
-        "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
-        "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases"),
+      resolvers ++= Dependencies.resolutionRepos,
 
       libraryDependencies ++= Seq("com.typesafe.akka" % "akka-actor" % "2.0.1",
         // Against Scalaz 7.0.6, available for Scala 2.10.4 and 2.11.2
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,5 +1,9 @@
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.10.1")
+resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+resolvers += "artifactory" at "http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"
 
 resolvers += "bigtoast-github" at "http://bigtoast.github.com/repo/"
 
-addSbtPlugin("com.github.bigtoast" % "sbt-thrift" % "0.7")
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.8.3")
+
+addSbtPlugin("com.github.bigtoast" % "sbt-thrift" % "0.6")

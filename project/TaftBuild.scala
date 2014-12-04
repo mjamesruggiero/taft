0d6cc9726e24927b0f1848261a160c7b86aaa6f6
mjamesruggiero <mjamesruggiero@gmail.com>
2014-12-03 22:29:21 -0800

    add necessary libraries
    * not sure about Jackson, but we'll leave that for now

--- project/TaftBuild.scala
+++ project/TaftBuild.scala
@@ -12,11 +12,54 @@ object Dependencies {
     "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
     "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases"
   )
+
+  object Versions {
+    // Java
+    val logging    = "1.1.3"
+    val httpCore   = "4.3"
+    val httpClient = "4.3.1"
+    val jacksonCore = "2.3.0"
+    val slf4j      = "1.7.5"
+    val awsSdk     = "1.6.10"
+
+    // Scala
+    val argot      = "1.0.1"
+    val config     = "1.0.2"
+    val scalaUtil  = "0.1.0"
+    val scalazon   = "0.5"
+    val scalaz = "0.5"
+
+    // Scala compile only for sbt-thrift.
+    val commonsLang3 = "3.1"
+    val thrift = "0.9.0"
+  }
+
+  object Libraries {
+    // Java
+    val logging     = "commons-logging"            %  "commons-logging" % Versions.logging
+    val httpCore    = "org.apache.httpcomponents"  %  "httpcore"        % Versions.httpCore
+    val httpClient  = "org.apache.httpcomponents"  %  "httpclient"      % Versions.httpClient
+    val jacksonCore = "com.fasterxml.jackson.core" % "jackson-core"     % Versions.jacksonCore
+    val slf4j       = "org.slf4j"                  % "slf4j-simple"     % Versions.slf4j
+    val awsSdk      = "com.amazonaws"              % "aws-java-sdk"     % Versions.awsSdk
+
+    // Scala
+    val argot       = "org.clapper"                %% "argot"           % Versions.argot
+    val config      = "com.typesafe"               %  "config"          % Versions.config
+    val scalaUtil   = "com.snowplowanalytics"      %  "scala-util"      % Versions.scalaUtil
+    val scalazon    = "io.github.cloudify"         %% "scalazon"        % Versions.scalazon
+    val scalaz      = "org.scalaz.stream"          %% "scalaz-stream"   % Versions.scalaz,
+
+    // Scala compile only for sbt-thrift.
+    val commonsLang3 = "org.apache.commons" % "commons-lang3" % Versions.commonsLang3 % "compile"
+    val thrift =    "org.apache.thrift"     % "libthrift" % Versions.thrift % "compile"
+  }
 }
 
 object TaftBuild extends Build {
+  import Dependencies._
+
   lazy val sbtAssemblySettings = assemblySettings ++ Seq(
-    // Slightly cleaner jar name
     jarName in assembly := {
       name + "-" + version + ".jar"
     }
@@ -35,13 +78,20 @@ object TaftBuild extends Build {
 
       resolvers ++= Dependencies.resolutionRepos,
 
-      libraryDependencies ++= Seq("com.typesafe.akka" % "akka-actor" % "2.0.1",
-        // Against Scalaz 7.0.6, available for Scala 2.10.4 and 2.11.2
-        "org.scalaz.stream" %% "scalaz-stream" % "0.5",
-        // Against Scalaz 7.1, available for Scala 2.10.4 and 2.11.2
-        "org.scalaz.stream" %% "scalaz-stream" % "0.5a",
-        // AWS
-        "com.github.seratch" %% "awscala" % "0.4.+")
+      libraryDependencies ++= Seq(
+        Libraries.logging,
+        Libraries.httpCore,
+        Libraries.httpClient,
+        Libraries.jacksonCore,
+        Libraries.argot,
+        Libraries.config,
+        Libraries.scalaUtil,
+        Libraries.scalazon,
+        Libraries.commonsLang3,
+        Libraries.thrift,
+        Libraries.slf4j,
+        Libraries.awsSdk,
+        Libraries.scalaz)
     )
   )
 }

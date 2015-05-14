3f83c849dbe8090d5ec05267382fe693036b38c6
mjamesruggiero <mjamesruggiero@gmail.com>
2015-05-13 17:02:30 -0700

    taft can parse a sequence of tweets

--- src/test/scala/com/mjamesruggiero/taft/datatypes/TweetsSpec.scala
+++ src/test/scala/com/mjamesruggiero/taft/datatypes/TweetsSpec.scala
@@ -10,6 +10,8 @@ object TweetsSpec extends Properties("Tweets") {
 
   def word(n: Int): Gen[String] = listOfN(n, alphaChar) map {_.mkString}
 
+  val tweetsInASearch = 20
+
   val smallInt = choose(1, 7)
 
   val username: Gen[String] = for {
@@ -34,10 +36,20 @@ object TweetsSpec extends Properties("Tweets") {
     text <- tweetText
   } yield Tweet(user, text)
 
+  val tweetSeq: Gen[SearchResults] = for {
+    statuses <- Gen.listOfN(tweetsInASearch, tweet)
+  } yield (SearchResults(statuses))
+
   property("can parse tweets") = forAll(tweet) { t =>
     val asString = s"""{ "user": { "screen_name": "${t.user}" }, "text": "${t.text}"}"""
     val testTweet = asString.asJson.convertTo[Tweet]
     testTweet.user.screen_name == t.user.screen_name
     testTweet.text == t.text
+  }
+
+  property("can parse a seq of tweets") = forAll(tweetSeq) { seqOfTweets =>
+    val testJson = seqOfTweets.toJson
+    val res = testJson.convertTo[SearchResults]
+    res.statuses.size == tweetsInASearch
   }
 }

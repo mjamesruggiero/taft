722e8d2e146fd8ff5e70fc4dca18cf84fe060eb2
mjamesruggiero <mjamesruggiero@gmail.com>
2015-05-26 20:46:04 -0700

    remove unused code

--- src/main/scala/com/mjamesruggiero/taft/Analyzer.scala
+++ src/main/scala/com/mjamesruggiero/taft/Analyzer.scala
@@ -2,6 +2,10 @@ package com.mjamesruggiero.taft
 
 import com.mjamesruggiero.taft.datatypes._
 
+sealed trait Message
+
+case object HaveEnough extends Message
+
 class Analyzer(tweet: Tweet) {
   def tokenize: List[String] = Tokenizer(tweet.text).eachWord
 
@@ -10,15 +14,4 @@ class Analyzer(tweet: Tweet) {
 
 object Analyzer {
   def apply(tweet: Tweet): Analyzer = new Analyzer(tweet)
-
-  class TweetPool(size: Int, contents: Seq[Tweet] = Vector()) {
-
-    private def copy(contents: Seq[Tweet]) = new TweetPool(size, contents)
-
-    def findBest: Option[(Tweet, TweetPool)] =
-      contents sortBy {_.text.length} match {
-        case Seq() => None
-        case Seq(head, tail@ _*) => Some((head, copy(tail)))
-      }
-  }
 }

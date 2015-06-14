352878cce9cb1863565e244b3da6de5effb53963
mjamesruggiero <mjamesruggiero@gmail.com>
2015-06-13 21:25:14 -0700

    starting to hate scalaz
    * made everything much more complicated than it needed to be

--- src/main/scala/com/mjamesruggiero/taft/Taft.scala
+++ src/main/scala/com/mjamesruggiero/taft/Taft.scala
@@ -8,27 +8,13 @@ import scala.io.Source
 import scala.util.Random
 import scalaz._, Scalaz._
 import scalaz.concurrent.{Task, Strategy}
-import scalaz.stream.{time, Process, Process1, async}
+import scalaz.stream._
 import spray.json._
 
 object Taft extends App {
   import TaftJsonProtocol._
 
   implicit val scheduler = Strategy.DefaultTimeoutScheduler
-
-  val staticFile = "/Users/michaelruggiero/code/mr/taft/src/test/resources/tweets.json"
-
-  case class SearchResults(tweets: Seq[Tweet])
-
-  def resultsToTweets: Process1[SearchResults, Tweet] =
-    Process.await1 flatMap { results: SearchResults =>
-      Process.emitAll(results.tweets)
-    }
-
-  def getRandomTweet(lot: List[Tweet]): Tweet = {
-    val random = new Random
-    lot(random.nextInt(lot.length))
-  }
 
   val rcp = new RedisClientPool("localhost", 6379)
 
@@ -45,26 +31,14 @@ object Taft extends App {
       _ <- Database.set(Keys.tweetKey(el), el.toJson.toString, rcp)
     } yield(el)
 
-  def countTokens(l: List[String]) = l.groupBy(identity).mapValues(_.length)
-
   val tokenizeTweet = (el: Tweet) => Task {
     Analyzer(el).tokenize
   }
 
-  val processTokens = (l: List[String]) => Task { println(countTokens(l)) }
-
-  val queue = async.boundedQueue[Tweet](100)
-
-  val enqueueProcess = time
-                        .awakeEvery(60.millis)
-                        .map(_ => getRandomTweet(tweetList))
-                        .to(queue.enqueue)
-
-  val dequeueProcess = queue
-                        .dequeue
-                        .evalMap(saveTweet)
-                        .evalMap(tokenizeTweet)
-                        .evalMap(processTokens)
-
-  (enqueueProcess merge dequeueProcess).run.run
+  def process: Unit = {
+    for (tweet <- tweetList) {
+      saveTweet(tweet).run
+    }
+  }
+  process
 }

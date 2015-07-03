1e03fd6aadf61b3aedcfe0d5abd9ab87ac044a8f
mjamesruggiero <mjamesruggiero@gmail.com>
2015-07-02 17:12:45 -0700

    idf

--- src/main/scala/com/mjamesruggiero/taft/Topic.scala
+++ src/main/scala/com/mjamesruggiero/taft/Topic.scala
@@ -8,4 +8,12 @@ object Topic {
     println(s"count is ${count} and length is ${len} and tf is ${tf}")
     tf
   }
+
+  def nContaining(word: String, documents: List[List[String]]): Int  = {
+    documents.filter(x => x contains word).length
+  }
+
+  def idf(word: String, documents: List[List[String]]): Double = {
+    scala.math.log(documents.length / (1 + nContaining(word, documents)))
+  }
 }
--- src/test/scala/com/mjamesruggiero/taft/TopicSpec.scala
+++ src/test/scala/com/mjamesruggiero/taft/TopicSpec.scala
@@ -15,5 +15,25 @@ class TopicSpec extends FlatSpec with Matchers {
     tf("goat", words) should equal(expected)
   }
 
+  "nContaining" should "count the number of token lists containing word" in {
+    val documents = List(
+      List("car", "boat", "train"),
+      List("car", "plane", "train"),
+      List("car", "boat", "hovercraft"))
+    nContaining("boat", documents) should equal(2)
+  }
 
+  "idf" should "count the log of documents over 1 + nContaining" in {
+    val documents = List(
+      List("car", "boat", "train"),
+      List("car", "plane", "train"),
+      List("car", "plane", "train"),
+      List("car", "plane", "train"),
+      List("car", "plane", "train"),
+      List("car", "plane", "train"),
+      List("car", "plane", "train"),
+      List("car", "boat", "hovercraft"))
+    val expected = 0.6931471805599453
+    idf("boat", documents) should equal(expected)
+  }
 }

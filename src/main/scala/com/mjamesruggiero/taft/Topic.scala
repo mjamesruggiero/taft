d6c11ac4040ebd7a9225dea39ad54b20754ca814
mjamesruggiero <mjamesruggiero@gmail.com>
2015-07-03 19:59:55 -0700

    tfidf

--- src/main/scala/com/mjamesruggiero/taft/Topic.scala
+++ src/main/scala/com/mjamesruggiero/taft/Topic.scala
@@ -1,19 +1,30 @@
 package com.mjamesruggiero.taft
 
+case class WordScore(word: String, score: Double)
+
 object Topic {
   def tf(word: String, tokens: List[String]): Double = {
     val count = tokens.count(_ == word).toDouble
     val len = tokens.length.toDouble
-    val tf = count / tokens.length
-    println(s"count is ${count} and length is ${len} and tf is ${tf}")
-    tf
+    count / tokens.length
   }
 
-  def nContaining(word: String, documents: List[List[String]]): Int  = {
-    documents.filter(x => x contains word).length
+  def nContaining(word: String, documents: List[List[String]]): Double = {
+    documents.map { doc =>
+      if (doc contains word) { 1.0 } else { 0.0 }
+    }.foldLeft(0.0)(_ + _)
   }
 
   def idf(word: String, documents: List[List[String]]): Double = {
-    scala.math.log(documents.length / (1 + nContaining(word, documents)))
+    val containing = nContaining(word, documents)
+    scala.math.log(documents.length.toDouble / (1.0 + containing))
+  }
+
+  def tfidf(word: String, tokens: List[String], documents: List[List[String]]): Double = {
+    tf(word, tokens) * idf(word, documents)
+  }
+
+  def sortedWordScores(scores: List[WordScore]): List[WordScore] = {
+    scores.sortWith(_.score < _.score)
   }
 }
--- src/test/scala/com/mjamesruggiero/taft/TopicSpec.scala
+++ src/test/scala/com/mjamesruggiero/taft/TopicSpec.scala
@@ -33,7 +33,43 @@ class TopicSpec extends FlatSpec with Matchers {
       List("car", "plane", "train"),
       List("car", "plane", "train"),
       List("car", "boat", "hovercraft"))
-    val expected = 0.6931471805599453
+    val expected = 0.9808292530117262
     idf("boat", documents) should equal(expected)
+  }
+
+  "tfidf" should "calculate the product of tf and if" in {
+    val doc1 = """Python is a 2000 made-for-TV horror movie directed by Richard
+    Clabaugh. The film features several cult favorite actors, including William
+    Zabka of The Karate Kid fame, Wil Wheaton, Casper Van Dien, Jenny McCarthy,
+    Keith Coogan, Robert Englund (best known for his role as Freddy Krueger in the
+    A Nightmare on Elm Street series of films), Dana Barron, David Bowe, and Sean
+    Whalen. The film concerns a genetically engineered snake, a python, that
+    escapes and unleashes itself on a small town. It includes the classic final
+    girl scenario evident in films like Friday the 13th. It was filmed in Los Angeles,
+    California and Malibu, California. Python was followed by two sequels: Python
+    II (2002) and Boa vs. Python (2004), both also made-for-TV films.""".split("\\s+").toList
+
+    val doc2 = """Python, from the Greek word (πύθων/πύθωνας), is a genus of
+    nonvenomous pythons[2] found in Africa and Asia. Currently, 7 species are
+    recognised.[2] A member of this genus, P. reticulatus, is among the longest
+    snakes known.""".split("\\s+").toList
+
+    val doc3 = """The Colt Python is a .357 Magnum caliber revolver formerly
+    manufactured by Colt's Manufacturing Company of Hartford, Connecticut.
+    It is sometimes referred to as a "Combat Magnum".[1] It was first introduced
+    in 1955, the same year as Smith &amp; Wesson's M29 .44 Magnum. The now discontinued
+    Colt Python targeted the premium revolver market segment. Some firearm
+    collectors and writers such as Jeff Cooper, Ian V. Hogg, Chuck Hawks, Leroy
+    Thompson, Renee Smeets and Martin Dougherty have described the Python as the
+    finest production revolver ever made.""".split("\\s+").toList
+
+    val doc1Scores = doc1.map { word =>
+      val score = tfidf(word, doc1, List(doc1, doc2, doc3))
+      WordScore(word, score)
+    }.toList
+
+    val sorted = Topic.sortedWordScores(doc1Scores)
+    val highest = sorted.last.word
+    highest should equal("made-for-TV")
   }
 }

5cb947fdcb65a3bd6a6dc6ade3b7b94cbbf97a9c
mjamesruggiero <mjamesruggiero@gmail.com>
2015-07-05 20:47:22 -0700

    dogfood your tokenizer

--- src/test/scala/com/mjamesruggiero/taft/TopicSpec.scala
+++ src/test/scala/com/mjamesruggiero/taft/TopicSpec.scala
@@ -38,7 +38,7 @@ class TopicSpec extends FlatSpec with Matchers {
   }
 
   "tfidf" should "calculate the product of tf and if" in {
-    val doc1 = """Python is a 2000 made-for-TV horror movie directed by Richard
+    val doc1 = Tokenizer("""Python is a 2000 made-for-TV horror movie directed by Richard
     Clabaugh. The film features several cult favorite actors, including William
     Zabka of The Karate Kid fame, Wil Wheaton, Casper Van Dien, Jenny McCarthy,
     Keith Coogan, Robert Englund (best known for his role as Freddy Krueger in the
@@ -47,21 +47,21 @@ class TopicSpec extends FlatSpec with Matchers {
     escapes and unleashes itself on a small town. It includes the classic final
     girl scenario evident in films like Friday the 13th. It was filmed in Los Angeles,
     California and Malibu, California. Python was followed by two sequels: Python
-    II (2002) and Boa vs. Python (2004), both also made-for-TV films.""".split("\\s+").toList
+    II (2002) and Boa vs. Python (2004), both also made-for-TV films.""").eachWord
 
-    val doc2 = """Python, from the Greek word (πύθων/πύθωνας), is a genus of
+    val doc2 = Tokenizer("""Python, from the Greek word (πύθων/πύθωνας), is a genus of
     nonvenomous pythons[2] found in Africa and Asia. Currently, 7 species are
     recognised.[2] A member of this genus, P. reticulatus, is among the longest
-    snakes known.""".split("\\s+").toList
+    snakes known.""").eachWord
 
-    val doc3 = """The Colt Python is a .357 Magnum caliber revolver formerly
+    val doc3 = Tokenizer("""The Colt Python is a .357 Magnum caliber revolver formerly
     manufactured by Colt's Manufacturing Company of Hartford, Connecticut.
     It is sometimes referred to as a "Combat Magnum".[1] It was first introduced
     in 1955, the same year as Smith &amp; Wesson's M29 .44 Magnum. The now discontinued
     Colt Python targeted the premium revolver market segment. Some firearm
     collectors and writers such as Jeff Cooper, Ian V. Hogg, Chuck Hawks, Leroy
     Thompson, Renee Smeets and Martin Dougherty have described the Python as the
-    finest production revolver ever made.""".split("\\s+").toList
+    finest production revolver ever made.""").eachWord
 
     val doc1Scores = doc1.map { word =>
       val score = tfidf(word, doc1, List(doc1, doc2, doc3))
@@ -70,6 +70,6 @@ class TopicSpec extends FlatSpec with Matchers {
 
     val sorted = doc1Scores.sortWith(_.score < _.score)
     val highest = sorted.last.word
-    highest should equal("made-for-TV")
+    highest should equal("films")
   }
 }
